3ad9746a0001662cc9ffd9f480a77af550a8775d
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-04-03 15:09:05 -0400

    Add layout & change indent of server.py

--- server.py
+++ server.py
@@ -58,29 +58,29 @@ engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
+    """
+    This function is run at the beginning of every web request 
+    (every time you enter an address in the web browser).
+    We use it to setup a database connection that can be used throughout the request.
 
-  The variable g is globally accessible.
-  """
-  try:
+    The variable g is globally accessible.
+    """
+    try:
     g.conn = engine.connect()
-  except:
+    except:
     print "uh oh, problem connecting to database"
     import traceback; traceback.print_exc()
     g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't, the database could run out of memory!
-  """
-  try:
+    """
+    At the end of the web request, this makes sure to close the database connection.
+    If you don't, the database could run out of memory!
+    """
+    try:
     g.conn.close()
-  except Exception as e:
+    except Exception as e:
     pass
 
 #
@@ -99,75 +99,72 @@ def teardown_request(exception):
 
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
+    """
+    request is a special object that Flask provides to access web request information:
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+    request.method:   "GET" or "POST"
+    request.form:     if the browser submitted a form, this contains the data in the form
+    request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
 
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
+    See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+    """
 
-  # DEBUG: this is debugging code to see what request looks like
-  # print request.args
+    # DEBUG: this is debugging code to see what request looks like
+    # print request.args
 
-  #
-  # example of a database query
-  #
-  # cursor = g.conn.execute("SELECT Name FROM Person")
-  # names = []
-  # for result in cursor:
-  #   names.append(result['name'])  # can also be accessed using result[0]
-  # cursor.close()
+    #
+    # example of a database query
+    #
+    # cursor = g.conn.execute("SELECT Name FROM Person")
+    # names = []
+    # for result in cursor:
+    #   names.append(result['name'])  # can also be accessed using result[0]
+    # cursor.close()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  # context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html")
+    #
+    # Flask uses Jinja templates, which is an extension to HTML where you can
+    # pass data to a template and dynamically generate HTML based on the data
+    # (you can think of it as simple PHP)
+    # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
+    #
+    # You can see an example template in templates/index.html
+    #
+    # context are the variables that are passed to the template.
+    # for example, "data" key in the context variable defined below will be 
+    # accessible as a variable in index.html:
+    #
+    #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+    #     <div>{{data}}</div>
+    #     
+    #     # creates a <div> tag for each element in data
+    #     # will print: 
+    #     #
+    #     #   <div>grace hopper</div>
+    #     #   <div>alan turing</div>
+    #     #   <div>ada lovelace</div>
+    #     #
+    #     {% for n in data %}
+    #     <div>{{n}}</div>
+    #     {% endfor %}
+    #
+    # context = dict(data = names)
 
 
-
+    #
+    # render_template looks in the templates/ folder for files.
+    # for example, the below file reads template/index.html
+    #
+    return render_template("index.html")
 
 if __name__ == "__main__":
-  import click
+    import click
 
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='127.0.0.1')
-  @click.argument('PORT', default=8080, type=int)
-  def run(debug, threaded, host, port):
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='127.0.0.1')
+    @click.argument('PORT', default=8080, type=int)
+    def run(debug, threaded, host, port):
     """
     This function handles command line parameters.
     Run the server using:
@@ -183,4 +180,4 @@ if __name__ == "__main__":
     print "running on %s:%d" % (HOST, PORT)
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
-  run()
+    run()
