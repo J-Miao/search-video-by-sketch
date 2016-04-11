a305255e45805ccdbfe2ca9b05120ca31188da8d
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-04-10 23:30:54 -0400

    Add tag search for the JSON API

---
+++ SketchLib/tag_parse.py
@@ -0,0 +1,15 @@
+import sys
+
+def tag_file_to_dict(filename):
+	f = open(filename, 'rb')
+	tag_str = f.read()
+	tag_indexes = {int(num_tag.split(' ')[0]): num_tag.split(' ')[1] for num_tag in tag_str.split('\n')}
+	return tag_indexes, sorted(tag_indexes)
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Plz input tag file"
+	else:
+		print tag_file_to_dict(sys.argv[1])
+
+
