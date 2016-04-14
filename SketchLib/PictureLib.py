9118ec4d2ffe2c1e21f21acc3eea702ad1133ed5
J-Miao <j.miao92@gmail.com>
2016-04-14 05:30:37 +0000

    holy shit

--- SketchLib/PictureLib.py
+++ SketchLib/PictureLib.py
@@ -3,11 +3,26 @@ from image_match.goldberg import ImageSignature
 
 import base64
 import numpy as np
+from math import sqrt
 
 gis = ImageSignature()
 #user_sketch_image = "../static/img/user_sketch_img.png"
 user_sketch_image = "/home/search-video-by-sketch/static/sketch-recognizer/data/sketches_sbsr/images/1.png"
-user_signature = None
+user_signature = np.array([])
+
+def distance(a,b):
+    suma = 0
+    print len(a), "lena"
+    print len(b), "lenb"
+    for item in a:
+	suma += item * item
+    sumb = 0
+    for item in b:
+	sumb += item * item
+    de = 0
+    for i in range (len(a)):
+	de += (a[i] - b[i]) * (a[i] - b[i])
+    return sqrt(de) / (sqrt(suma) + sqrt(sumb))
 
 def save_to_png(base64_str, file_name):
     f = open(file_name, 'w')
@@ -16,17 +31,18 @@ def save_to_png(base64_str, file_name):
     return file_name
 
 def compare(result_dict):
-    print "result_dict", result_dict	
-    return gis.normalized_distance(result_dict['signature'], user_signature)
+    print "result_dict", result_dict
+    #return distance(result_dict['signature'].tolist(), user_signature)	
+    return gis.normalized_distance(np.fromiter(result_dict['signature'],dtype='int8'), user_signature)
 
 def picture_matcher(mongo, sketch_tag, user_sketch_pic_base64, page_idx=0):
     call_back = mongo.db.vdb_images.find()
     #user_signature = gis.generate_signature(np.frombuffer(base64.decodestring(user_sketch_pic_base64), dtype=int), bytestream=True)
     results = []
+    global user_signature 
     user_signature = gis.generate_signature(user_sketch_image)
-    print "##### user_signature ###", user_signature
     for document in call_back:
     	if sketch_tag in document['tags']:
-           results.append({'pic': document['base64'], 'signature': np.fromiter(document['signature'], dtype=int)})
+           results.append({'pic': document['base64'], 'signature': document['signature']})
     return sorted(results, key=compare)
 
