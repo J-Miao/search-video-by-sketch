3534f3913bae27c6c3dd288cabd67cf910868d1d
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-04-13 22:21:23 -0400

    Fix bug

--- SketchLib/PictureLib.py
+++ SketchLib/PictureLib.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 
-def picture_matcher(mongo, tag, page_idx=0):
+def picture_matcher(mongo, sketch_tag, page_idx=0):
     call_back = mongo.db.vdb_images.find()
     results = []
     for document in call_back:
