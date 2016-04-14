84a8b432483fec810cdcbcdadf8d6437cd504cd4
J-Miao <j.miao92@gmail.com>
2016-04-14 06:32:37 +0000

    Modify for click original img

--- server.py
+++ server.py
@@ -100,7 +100,7 @@ def get_sketches():
         return redirect("/")
     else:
         sketch_binary_str = request.form["sketch"]
-        fname = output_sketch + "-" + datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        fname = output_sketch #+ "-" + datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
         save_to_png(sketch_binary_str, fname)
         results = sketch_recogniser(fname)
         return jsonify({"sketches": results})
@@ -110,9 +110,10 @@ def get_pictures():
     sketch_tag = request.form.get('tag', None)
     sketch_pic_base64 = request.form.get('sketch_pic', "")
     page_idx = int(request.form.get('page', 0))
+    sketch_file_path = request.form.get('sketch_file_path', "")
     global picture_results
-    if page_idx == 0 and len(picture_results) == 0:
-       picture_results = picture_matcher(mongo, sketch_tag, sketch_pic_base64)
+    #if page_idx == 0 and len(picture_results) == 0:
+    picture_results = picture_matcher(mongo, sketch_tag, sketch_pic_base64, sketch_file_path)
     return jsonify({"pictures": picture_results[page_idx:page_idx + 20]})
     # if page_idx == 0:
     #    global picture_results
