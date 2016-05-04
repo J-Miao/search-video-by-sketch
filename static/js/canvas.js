2bddfd7075d72f24a9ce377e1f6aea79d20f9f82
J-Miao <j.miao92@gmail.com>
2016-05-04 19:30:11 +0000

    hh

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -858,8 +858,8 @@ function addRect(x, y, w, h, fillImg) {
   var pat = context[1].createPattern(fillImg, 'no-repeat');
   rect.x = x;
   rect.y = y;
-  rect.w = w;
-  rect.h = h;
+  rect.w = w/3;
+  rect.h = h/3;
   rect.fill = pat;
   boxes2.push(rect);
   invalidate();
